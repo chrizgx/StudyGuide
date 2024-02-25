68c3db86b1dffb98e5fae01ce4054a71cb4f24a1
Christos Georgoulis <102369753+chrizgx@users.noreply.github.com>
2024-02-25 21:14:35 +0000

    Create and test basic view of ngFor and filter functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { DataService } from './services/data.service';
+import { FilterService } from './services/filter.service';
+
+import { Course } from './models/course.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  dataService: DataService = inject(DataService);
+  filterService: FilterService = inject(FilterService);
+
   title = 'frontend';
   viewSelectedCourses: boolean = false;
-  sorting: 1 | 2 | 3 | 4 = 1;
+  sorting: 0 | 1 | 2 | 3 = 0;
+  section: any = {};
+
+  ngOnInit() {
+    this.getSection();
+  }
+
+  getSection() {
+    // this.section = this.filterService.filters[this.sorting];
+  }
+
+  onSortingChange() {
+    this.getSection();
+    console.log(this.sorting);
+  }
+
+  getValues(object: any): Course[] {
+    console.log(Object.values(object)[1]);
+    return Object.values(object);
+  }
+
 }
